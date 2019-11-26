e85ca2823d05aec0a1c3a085ca2fa96355a2989b
Lissette Quebrada <lissque604@gmail.com>
2019-11-26 16:37:25 -0500

    HOLa
    fghjk

--- src/co/uniquindio/mundo/Respuesta.java
+++ src/co/uniquindio/mundo/Respuesta.java
@@ -1,22 +1,7 @@
 package co.uniquindio.mundo;
 
 public class Respuesta {
-	private int r1;
-	private int r2;
-	private int r3;
-	private int r4;
-	private int r5;
 	
-	public Respuesta(int respuesta) {
-		switch (res) {
-		case value:
-			
-			break;
-
-		default:
-			break;
-		}
-	}
 	
 	
 }
