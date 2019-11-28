05cf3d522020586b7bec134386b897337891ac40
Luisa Fernanda Cotte <luisacotte18@hotmail.com>
2019-11-28 17:15:34 -0500

    TARIMA

--- src/co/uniquindio/interfaz/Secciones.java
+++ src/co/uniquindio/interfaz/Secciones.java
@@ -15,6 +15,8 @@ import co.uniquindio.mundo.Escenario;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
 
 public class Secciones extends JFrame implements ActionListener {
 
@@ -38,56 +40,70 @@ public class Secciones extends JFrame implements ActionListener {
 		this.anterior = anterior;
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 409, 200);
+		setBounds(100, 100, 500, 400);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 218, 185));
 		contentPane.setBorder(new LineBorder(new Color(64, 64, 64), 5, true));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		btnM1 = new JButton("Platea");
-		btnM1.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		btnM1.setBounds(20, 31, 89, 61);
+		btnM1 = new JButton("PLATEA");
+		btnM1.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnM1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnM1.setFont(new Font("Gabriola", Font.BOLD, 25));
+		btnM1.setBounds(24, 108, 128, 124);
 		contentPane.add(btnM1);
 		btnM1.addActionListener(this);
 
-		btnM2 = new JButton("Club Fans");
-		btnM2.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		btnM2.setBounds(145, 90, 97, 88);
+		btnM2 = new JButton("CLUB DE FANS");
+		btnM2.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnM2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnM2.setFont(new Font("Gabriola", Font.BOLD, 25));
+		btnM2.setBounds(169, 141, 165, 103);
 		btnM2.addActionListener(this);
 		contentPane.add(btnM2);
 
-		btnM3 = new JButton("Platea");
-		btnM3.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		btnM3.setBounds(278, 31, 89, 61);
+		btnM3 = new JButton("PLATEA");
+		btnM3.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnM3.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnM3.setFont(new Font("Gabriola", Font.BOLD, 25));
+		btnM3.setBounds(349, 108, 128, 124);
 		btnM3.addActionListener(this);
 		contentPane.add(btnM3);
 
-		btnM4 = new JButton("Segundo Piso");
-		btnM4.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		btnM4.setBounds(20, 103, 115, 61);
+		btnM4 = new JButton("SEGUNDO PISO");
+		btnM4.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnM4.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnM4.setFont(new Font("Gabriola", Font.BOLD, 23));
+		btnM4.setBounds(93, 255, 149, 82);
 		btnM4.addActionListener(this);
 		contentPane.add(btnM4);
 
 		btnM5 = new JButton("Segundo Piso");
-		btnM5.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		btnM5.setBounds(252, 103, 115, 61);
+		btnM5.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnM5.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnM5.setFont(new Font("Gabriola", Font.BOLD, 23));
+		btnM5.setBounds(266, 255, 149, 82);
 		btnM5.addActionListener(this);
 		contentPane.add(btnM5);
 
-		btnVolver = new JButton("\u2190");
+		btnVolver = new JButton("\u21A9");
+		btnVolver.setFont(new Font("Arial Unicode MS", Font.BOLD, 25));
+		btnVolver.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnVolver.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnVolver.addActionListener(this);
-		btnVolver.setBounds(346, 166, 53, 23);
+		btnVolver.setBounds(202, 348, 97, 29);
 		contentPane.add(btnVolver);
 
 		JLabel lblTarima = new JLabel("TARIMA");
-		lblTarima.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-		lblTarima.setBackground(Color.WHITE);
+		lblTarima.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+		lblTarima.setBackground(new Color(233, 150, 122));
 		lblTarima.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTarima.setBorder(new LineBorder(Color.GRAY, 3, true));
-		lblTarima.setFont(new Font("NSimSun", Font.BOLD, 25));
+		lblTarima.setFont(new Font("Gabriola", Font.BOLD, 35));
 		lblTarima.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblTarima.setBounds(119, 11, 149, 52);
+		lblTarima.setBounds(169, 27, 165, 103);
 		contentPane.add(lblTarima);
 	}
 
